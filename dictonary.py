18540bd9cce8179ccf01584128926e326efe2837
mahesh483 <61460379+mahesh483@users.noreply.github.com>
2020-06-05 13:30:44 +1000

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+dict={'keys':["shanghai", "istanbul", "karachi", "mumbai"],'Values':["17.8", "13.3", "13.0", "12.5"]}
+dist=pd.DataFrame(dict)
+
